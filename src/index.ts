86f66714d8e48eb01163badddf0024236ef6f657
LittleBoBo-beep <845734834@qq.com>
2021-05-06 15:25:24 +0800

    重新构建

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-import Table from '@/Table'
-import '@/index.less'
+// import Table from '@/Table'
+// import '@/index.less'
+import '@/model/index.less'
+import Table from '@/model/table'
+
 export default Table
---
+++ src/model/body.ts
@@ -0,0 +1,7 @@
+import Cell from './cell'
+export default class Body extends Cell {
+    constructor() {
+        super()
+    }
+    init() {}
+}
